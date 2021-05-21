27ba592d5e032f0ca23862b4abdbdb6a99412f6e
okorkov <alex.okorkov@gmail.com>
2021-05-21 15:30:57 -0700

    fixing heroku corse

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module VChefBackend
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://v-chef.web.app/', 'http://localhost:3000'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get]
       end
     end
