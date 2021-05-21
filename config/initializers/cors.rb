032bdfd46b842fb9f71477b8ba39ce2379b31e3d
okorkov <alex.okorkov@gmail.com>
2021-05-21 15:01:45 -0700

    cors config testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://shop-easy-online.web.app', 'http://localhost:3001'
+    origins 'https://v-chef.web.app/', 'http://localhost:3001'
     resource '*',
       headers: :any,
       methods: [:get],
