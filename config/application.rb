b295846ff77ee6fbea4ca3ba6bf83f110816e5fe
okorkov <alex.okorkov@gmail.com>
2021-05-21 15:25:58 -0700

    changed port to 300 from 3001 for cors

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module VChefBackend
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://v-chef.web.app/', 'http://localhost:3001'
+        origins 'https://v-chef.web.app/', 'http://localhost:3000'
         resource '*', :headers => :any, :methods => [:get]
       end
     end
