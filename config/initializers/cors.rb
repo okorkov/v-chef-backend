b1523402aa990c0118cb9e7dbedc284643387336
okorkov <alex.okorkov@gmail.com>
2021-05-21 16:38:02 -0700

    testing moving cors to config.ru

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,9 +16,13 @@
 
 # end
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :options]
-  end
-end
+# config.middleware.insert_before "ActionDispatch::Static", "Rack::Cors" do
+#   allow do
+#       origins '*'
+#       resource '*',
+#           headers: :any,
+#           methods: [:get, :post, :delete, :put, :patch, :options, :head],
+#           max_age: 0,
+#           expose: :location
+#       end
+#   end
