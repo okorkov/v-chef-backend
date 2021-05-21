f68e36936992e2f6f6c97f5b578cc4ef17dfe992
okorkov <alex.okorkov@gmail.com>
2021-05-21 15:56:54 -0700

    removed cors gem and config to test heroku

--- config/application.rb
+++ config/application.rb
@@ -39,17 +39,17 @@ module VChefBackend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    # config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'https://v-chef.web.app/', 'http://localhost:3001'
-        resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins 'https://v-chef.web.app/', 'http://localhost:3001'
+    #     resource '*',
+    #     headers: :any,
+    #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    #     credentials: true
+    #   end
+    # end
     
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,16 +5,16 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
 
-  allow do
-    origins 'https://v-chef.web.app/', 'http://localhost:3001'
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
+#   allow do
+#     origins 'https://v-chef.web.app/', 'http://localhost:3001'
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#       credentials: true
+#   end
 
-end
+# end
 
